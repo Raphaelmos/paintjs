2d395e765ba5221027d62aaf01d4e0f4e6170478
Raphaelmos <96924364+Raphaelmos@users.noreply.github.com>
2022-01-14 15:39:05 +0100

    Modification déplacement souris et couleurs
    Ajout et exportation dessin

--- paintjs/app.js
+++ paintjs/app.js
@@ -1,40 +1,95 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
-ctx.lineWidtx = 2.5;
-ctx.strokeStyle = '#2c2c2c';
+const INITIAL_COLOR = '#2c2c2c';
+const CANVAS_SIZE = 800;
 
-let painting = false
+canvas.height = CANVAS_SIZE;
+canvas.width = CANVAS_SIZE;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+
+ctx.lineWidth = 2.5;
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+
+let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
 }
 
-function startPaiting(){
-    painting = true 
+function startPainting(){
+    painting = true;
 }
 
-function onMouseMove(event) {
+function onMouseMove(event){
     x = event.offsetX;
     y = event.offsetY;
-    if(!painting) {
+    if (!painting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
     }
 }
 
-function onMouseDown(event) {
-    painting = true 
+function onMouseDown(event){
+    painting = true;
 }
 
-
-function onMouseLeave(event){
-    stopPainting()
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
+
+function handleRangeChange(event){
+    const rangeValue = event.target.value;
+    ctx.lineWidth = rangeValue;
+}
+
+function handleModeClick(){
+    if (filling === true) {
+        filling = false;
+        mode.innerText = 'Remplir';
+    } else {
+        filling = true;
+        mode.innerText = 'Peindre';
+    }
+}
+
+function handleCanvasClick(){
+    if (filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = "Exportation du dessin";
+    link.click();
+}
+
 
 if (canvas) {
-    canvas.addEventListener('mousemove',onMouseMove);
+    canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', stopPainting());
-    canvas.addEventListener('mouseleave', stopPainting());
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave', stopPainting);
+    canvas.addEventListener('click', handleCanvasClick);
+    canvas.addEventListener('contextmenu', handleCM);
 }
